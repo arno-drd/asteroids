ea028a024a8eed4779e0e9f5ed4420ab7a45d492
arno-drd <arnod69001@gmail.com>
2025-01-25 17:00:47 +0100

    added a screen display, + .gitignore file

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    while True:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                #checks if the user closes the pygame tab, it will quit the game if they do
+                return
+        screen.fill((000,000,000))
+        pygame.display.flip()
 
 
 
