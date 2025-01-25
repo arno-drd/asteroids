73fc12d22e44ed37935e7832a8e54897342c5d1d
arno-drd <arnod69001@gmail.com>
2025-01-25 18:05:10 +0100

    adds a clock that limit fps to 60, and a delta value to track time

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ from constants import *
 
 def main():
     pygame.init()
+    #set up a delta and a Clock for fps gestion
+    dt = 0 
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 #checks if the user closes the pygame tab, it will quit the game if they do
                 return
         screen.fill((000,000,000))
         pygame.display.flip()
+        #using the pygame.time.Clock.tick() function limits the fps to 60; Also save the time that past in millisecond in our delta value
+        dt = clock.tick(60)/1000
 
 
 
